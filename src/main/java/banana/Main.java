553f963466e2748f041557da3dd3e82baa5653fb
Your Name <you@example.com>
2021-03-17 22:33:19 +0300

    Все очень плохо

---
+++ src/main/java/banana/Main.java
@@ -0,0 +1,11 @@
+package banana;
+
+import banana.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        RunnerConfig runner_config = new RunnerConfig();
+        runner_config.run_config("1");
+    }
+}
--- src/main/java/banana/RunnerConfig.java
+++ src/main/java/banana/RunnerConfig.java
@@ -4,15 +4,15 @@ import org.ini4j.*;
 import java.io.*;
 
 public class RunnerConfig {
-    String default_config_path = "C:\\Users\\andre\\OneDrive\\Рабочий стол\\config.ini"
-    public static void main(String args[]) {
-
-        try {
-            // Execute command
-            String command = "cmd /c start calc";
-            Process proc = Runtime.getRuntime().exec(command);
-
-        } catch (IOException e) {
-        }
+    public static void main(String args[]) throws IOException {
+    //
+    }
+    public void run_config(String config_name) throws IOException {
+        String default_config_path = "D:\\config.ini";
+        Wini configs_base = new Wini(new File(default_config_path));
+        int app_number = 0;
+        System.out.println("cmd /c start " + configs_base.get(config_name, String.valueOf(app_number)));
+        String command = "cmd /c start " + configs_base.get(config_name, String.valueOf(app_number));
+        Process proc = Runtime.getRuntime().exec(command);
     }
 }
