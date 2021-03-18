85eea3364c72b01e777d734a99b812a219271ff1
Your Name <you@example.com>
2021-03-18 21:35:27 +0300

    Зачем я это начинал.................

--- src/main/java/banana/Main.java
+++ src/main/java/banana/Main.java
@@ -5,7 +5,12 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        for ( int i = 0;i<args.length;i++) {
+            System.out.println(args[i]);
+        }
         RunnerConfig runner_config = new RunnerConfig();
-        runner_config.run_config("1");
+        //runner_config.run_config("1");
+        //runner_config.make_config("2", new String[]{"C:\\Users\\andre\\OneDrive\\Рабочий стол\\programs\\books\\59192_8f10f9c64c0b84b8fbb177e8da728b9d.pdf", "C:\\Users\\andre\\OneDrive\\Рабочий стол\\programs\\books\\800263_b5ee61913d823835c74e2cbd1b4b3382.pdf"});
+        runner_config.rename_config("2", "DAS");
     }
 }
--- src/main/java/banana/RunnerConfig.java
+++ src/main/java/banana/RunnerConfig.java
@@ -3,16 +3,38 @@ package banana;
 import org.ini4j.*;
 import java.io.*;
 
+
 public class RunnerConfig {
-    public static void main(String args[]) throws IOException {
-    //
+    protected static String default_config_path = "D:\\config.ini";
+
+    public static void main(String[] args) throws IOException {
+        //
     }
     public void run_config(String config_name) throws IOException {
-        String default_config_path = "D:\\config.ini";
-        Wini configs_base = new Wini(new File(default_config_path));
-        int app_number = 0;
-        System.out.println("cmd /c start " + configs_base.get(config_name, String.valueOf(app_number)));
-        String command = "cmd /c start " + configs_base.get(config_name, String.valueOf(app_number));
-        Process proc = Runtime.getRuntime().exec(command);
+        Wini configs_ini = new Wini(new File(default_config_path));
+
+        for (int app_number = 0;;app_number++) {
+            String app_path = configs_ini.get(config_name, String.valueOf(app_number));
+            if (app_path != null) {
+                String command = "cmd /c start " + app_path;
+                Process proc = Runtime.getRuntime().exec(command);
+            } else {
+                break;
+            }
+        }
+    }
+    public void make_config(String config_name, String[] apps_dirs) throws IOException {
+        Wini configs_ini = new Wini(new File(default_config_path));
+
+        for (int i = 0; i < apps_dirs.length; i++) {
+            configs_ini.put(config_name, String.valueOf(i), apps_dirs[i]);
+        }
+        configs_ini.store();
+    }
+
+    public void remove_config(String config_name) throws IOException {
+        Wini configs_ini = new Wini(new File(default_config_path));
+        configs_ini.remove(configs_ini.get(config_name));
+        configs_ini.store();
     }
 }
