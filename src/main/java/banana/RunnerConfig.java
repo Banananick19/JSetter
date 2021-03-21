abb9c527ddc3a9e53b779d6018ed0249d1b6e987
Your Name <you@example.com>
2021-03-21 21:47:18 +0300

    ConfigParser API

--- src/main/java/banana/RunnerConfig.java
+++ src/main/java/banana/RunnerConfig.java
@@ -24,25 +24,35 @@ public class RunnerConfig {
 
     }
 
-    public static void make_config(String config_name, String[] apps) {
-        //
+    public static void make_config(String config_name, String[] apps) throws Exception {
+        ConfigParser config  = new ConfigParser();
+        config.read(new File(default_config_path));
+        config.addSection(config_name);
+
+        for(int i = 0; i<apps.length; i++) {
+            config.set(config_name, String.valueOf(i), apps[i]);
+        }
+        config.write(new File(default_config_path));
     }
 
     public static void append_to_config(String config_name, String[] apps) throws Exception {
         int index = 0;
         ConfigParser config  = new ConfigParser();
-        config.read(new InputStreamReader(new FileInputStream(new File(default_config_path)), "UTF-8"));
+        config.read(new File(default_config_path));
         for (String key : config.options(config_name)) index++;
 
 
-        for (String app: apps) {
+        for (String app : apps) {
             config.set(config_name, String.valueOf(index), app);
             index++;
         }
         config.write(new File(default_config_path));
     }
 
-    public static void remove_config(String config_name) {
-        //
+    public static void remove_config(String config_name) throws Exception {
+        ConfigParser config  = new ConfigParser();
+        config.read(new File(default_config_path));
+        config.removeSection(config_name);
+        config.write(new File(default_config_path));
     }
 }
